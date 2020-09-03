df8df669ac621e2340e9057b12bc4d0dd0e9e71f
ecz5032 <ecz5032@psu.edu>
2020-09-03 16:10:58 +0000

    a

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ if unit == "F" or unit == "f":
   tempC = temp
   tempC = float(tempC)
   tempF = (tempC -32 ) * 5/9
-  print(str(tempC) + "° in Celsius is equivalent to " + str(tempF) + "° Fahrenheit.")
+  print(str(tempC) + "° in Fahrenheit is equivalent to " + str(tempF) + "° Celsius.")
 if unit == "C" or unit == "c":
   tempF = temp
   tempF = float(tempF)
   tempC = (tempF *9/5) + 32
-  print(str(tempF) + "° in Fahrenheit is equivalent to " + str(tempC) + "° Celsius.")
+  print(str(tempF) + "° in Celsius is equivalent to " + str(tempC) + "° Fahrenheit.")
 else:
     print(f"Invalid unit ({unit}).")
 
